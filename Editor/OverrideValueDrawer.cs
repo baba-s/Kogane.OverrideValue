93c72deb2cc626d7b07e540983208f6173e2e2bc
baba-s <shotababa1206@gmail.com>
2022-09-25 12:20:04 +0900

    fix

--- Editor/OverrideValueDrawer.cs
+++ Editor/OverrideValueDrawer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Kogane.Internal
 {
     [CustomPropertyDrawer( typeof( OverrideValue<> ), true )]
-    internal sealed class OverrideValueBaseDrawer : PropertyDrawer
+    internal sealed class OverrideValueDrawer : PropertyDrawer
     {
         public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
         {
